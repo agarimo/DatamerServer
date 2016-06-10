5074e2a1bc286afe508462d554822835619cf75b
Agarimo <agarimosoft@gmail.com>
2016-06-10 14:15:22 +0200

    WRK 201606010

--- src/server/UI/WinC.java
+++ src/server/UI/WinC.java
@@ -7,10 +7,12 @@ package server.UI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,17 +21,38 @@ import javafx.scene.control.Label;
 public class WinC implements Initializable {
     
     @FXML
-    private Label label;
+    private TableView tabla;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TableColumn tareaCL;
     
+    @FXML
+    private TableColumn propietarioCL;
+    
+    @FXML
+    private TableColumn estadoCL;
+    
+    @FXML
+    private TableColumn porcentajeCL;
+    
+    @FXML
+    private TextField tfServer;
+    
+    @FXML
+    private TextField tfTasker;
+    
+    @FXML
+    private MenuItem miCerrar;
+    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        initTable();
         // TODO
     }    
+    
+    public void initTable(){
+        
+    }
     
 }
